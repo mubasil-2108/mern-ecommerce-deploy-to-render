ef2eedb5991c37ed3f68b8e43230884776e98c0b
mubasil-2108 <mubasilbehzad234@gmail.com>
2025-02-18 10:58:47 +0500

    all changes added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000,
+  },
+  define: {
+    'process.env': {},
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
